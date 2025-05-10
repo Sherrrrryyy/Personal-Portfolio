ca9c84ffbc30209eca4f66f6bd8eff6840a3171d
Sherrrrryyy <Shaheerkhan090909@gmail.com>
2025-05-10 16:52:46 +0500

    refactor: restructure Home component by integrating Main section and adjusting Header layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <>
       <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-        <div className="container flex h-16 p-5 items-center justify-between">
+        <div className="container flex h-16  p-8 items-center justify-between">
           <Link href="/" className="flex items-center gap-2 font-semibold">
             <span>DevShaheer</span>
           </Link>
